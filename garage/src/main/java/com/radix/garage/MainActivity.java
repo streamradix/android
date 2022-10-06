ad95fda6b0ed2b13b684f413d4740fa812b008da
streamradix <111672276+streamradix@users.noreply.github.com>
2022-10-06 18:44:39 +0900

    Garage > Build: use viewBinding

--- garage/src/main/java/com/radix/garage/MainActivity.java
+++ garage/src/main/java/com/radix/garage/MainActivity.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.radix.garage.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+	private ActivityMainBinding bind;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		bind = ActivityMainBinding.inflate(getLayoutInflater());
+		setContentView(bind.getRoot());
 	}
 }

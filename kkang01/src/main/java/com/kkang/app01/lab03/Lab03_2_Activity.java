eae4db318ced916b15df0b10fc27df71d070691b
streamradix <111672276+streamradix@users.noreply.github.com>
2022-10-09 18:15:24 +0900

    Kkang > Lab03_2
    깡샘: create module > Lab03 - 사용자 인터페이스
    Lab03_1 - java 로 화면 구성
    Lab03_2 - layout.xml 로 화면 구성

---
+++ kkang01/src/main/java/com/kkang/app01/lab03/Lab03_2_Activity.java
@@ -0,0 +1,16 @@
+package com.kkang.app01.lab03;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.kkang.app01.R;
+
+public class Lab03_2_Activity extends AppCompatActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lab03_2);
+	}
+}

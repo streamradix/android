a37fe859b2ee3095563444ac66b3f26399762211
streamradix <111672276+streamradix@users.noreply.github.com>
2022-10-15 02:44:46 +0900

    Codelabs > Log statements
    Course: Android Developer Fundamentals
    Unit 1 > Lesson 1.1 > Task 6
    
    Verbose
    - Log.v(String, String)
    - Log.v(String, String, Throwable)
    
    Debug
    - Log.d(String, String)
    - Log.d(String, String, Throwable)
    
    Info
    - Log.i(String, String)
    - Log.i(String, String, Throwable)
    
    Warn
    - Log.w(String, String)
    - Log.w(String, String, Throwable)
    - Log.w(String, Throwable)
    
    Error
    - Log.e(String, String)
    - Log.e(String, String, Throwable)

--- codelabs_java_developers/src/main/java/com/codelabs/java_developers/fundamentals/Fundamentals011.java
+++ codelabs_java_developers/src/main/java/com/codelabs/java_developers/fundamentals/Fundamentals011.java
@@ -1,16 +1,30 @@
 package com.codelabs.java_developers.fundamentals;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.codelabs.java_developers.R;
 
 public class Fundamentals011 extends AppCompatActivity {
+	private static final String LOG_TAG = Fundamentals011.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fundamentals011);
+
+		Log.v("Fundamentals011", "Hello World"); // v: Verbose
+		Log.d("Fundamentals011", "Hello World"); // d: Debug
+		Log.i("Fundamentals011", "Hello World"); // i: Info
+		Log.w("Fundamentals011", "Hello World"); // w: Warn
+		Log.e("Fundamentals011", "Hello World"); // e: Error
+
+		Log.v(LOG_TAG, "Hello World"); // v: Verbose
+		Log.d(LOG_TAG, "Hello World"); // d: Debug
+		Log.i(LOG_TAG, "Hello World"); // i: Info
+		Log.w(LOG_TAG, "Hello World"); // w: Warn
+		Log.e(LOG_TAG, "Hello World"); // e: Error
 	}
 }

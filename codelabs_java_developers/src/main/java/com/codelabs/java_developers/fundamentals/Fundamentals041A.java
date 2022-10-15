27615df1f69d4308027de1793bbb062d35f21f2b
streamradix <111672276+streamradix@users.noreply.github.com>
2022-10-15 20:58:23 +0900

    Codelabs > Droid Cafe: Menus and pickers - Menu
    Course: Android Developer Fundamentals
    Unit 2 > Lesson 4.3 > Task 3
    
    set Theme of Activity to NoActionBar
    - style > item: windowActionBar = false
    - style > item: windowNoTitle   = true
    
    res > menu > xml file: menu item configuration
    - orderInCategory : priority shown
    - showAsAction    : icon display rule
    
    onCreateOptionsMenu(Menu) of Activity
    - inflates the menu, and adds items to the action bar
    
    onOptionsItemSelected(MenuItem) of Activity
    - app bar item click handler

--- codelabs_java_developers/src/main/java/com/codelabs/java_developers/fundamentals/Fundamentals041A.java
+++ codelabs_java_developers/src/main/java/com/codelabs/java_developers/fundamentals/Fundamentals041A.java
@@ -2,10 +2,13 @@ package com.codelabs.java_developers.fundamentals;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.codelabs.java_developers.R;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -19,6 +22,9 @@ public class Fundamentals041A extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fundamentals041_a);
 
+		Toolbar toolbar = findViewById(R.id.toolbar);
+		setSupportActionBar(toolbar);
+
 		FloatingActionButton fab = findViewById(R.id.fab);
 		fab.setOnClickListener(new View.OnClickListener() {
 			@Override
@@ -28,6 +34,54 @@ public class Fundamentals041A extends AppCompatActivity {
 				startActivity(intent);
 			}
 		});
+	}
+
+	/**
+	 * Inflates the menu, and adds items to the action bar if it is present.
+	 *
+	 * @param menu Menu to inflate.
+	 * @return Returns true if the menu inflated.
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.fund041a_menu, menu);
+		return true;
+	}
+
+	/**
+	 * Handles app bar item clicks.
+	 *
+	 * @param item Item clicked.
+	 * @return True if one of the defined items was clicked.
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		// This comment suppresses the Android Studio warning about simplifying
+		// the return statements.
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_order) {
+			Intent intent = new Intent(Fundamentals041A.this, Fundamentals041B.class);
+			intent.putExtra(EXTRA_MESSAGE, mOrderMessage);
+			startActivity(intent);
+			return true;
+		} else if (id == R.id.action_status) {
+			displayToast(getString(R.string.action_status_message));
+			return true;
+		} else if (id == R.id.action_favorites) {
+			displayToast(getString(R.string.action_favorites_message));
+			return true;
+		} else if (id == R.id.action_contact) {
+			displayToast(getString(R.string.action_contact_message));
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
 	}
 
 	/**

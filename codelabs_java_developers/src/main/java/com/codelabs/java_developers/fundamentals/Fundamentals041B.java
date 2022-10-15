b679337d9b6c1048248d883484c312fd0e50572b
streamradix <111672276+streamradix@users.noreply.github.com>
2022-10-15 20:25:11 +0900

    Codelabs > Droid Cafe: Input Controls - RadioButton
    Course: Android Developer Fundamentals
    Unit 2 > Lesson 4.2 > Task 2 and Challenge
    
    RadioGroup and RadioButton
    set Default checked button

--- codelabs_java_developers/src/main/java/com/codelabs/java_developers/fundamentals/Fundamentals041B.java
+++ codelabs_java_developers/src/main/java/com/codelabs/java_developers/fundamentals/Fundamentals041B.java
@@ -2,7 +2,10 @@ package com.codelabs.java_developers.fundamentals;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,5 +23,36 @@ public class Fundamentals041B extends AppCompatActivity {
 
 		TextView textView = findViewById(R.id.order_textview);
 		textView.setText(message);
+	}
+
+	public void onRadioButtonClicked(View view) {
+		// Is the button now checked?
+		boolean checked = ((RadioButton) view).isChecked();
+
+		// Check which radio button was clicked.
+		switch (view.getId()) {
+			case R.id.sameday:
+				if (checked)
+					// Same day service
+					displayToast(getString(R.string.same_day_messenger_service));
+				break;
+			case R.id.nextday:
+				if (checked)
+					// Next day delivery
+					displayToast(getString(R.string.next_day_ground_delivery));
+				break;
+			case R.id.pickup:
+				if (checked)
+					// Pick up
+					displayToast(getString(R.string.pick_up));
+				break;
+			default:
+				// Do nothing.
+				break;
+		}
+	}
+
+	private void displayToast(String message) {
+		Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
 	}
 }

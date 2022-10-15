231eced788a31429ffd8501bc54beb3dcf397a86
streamradix <111672276+streamradix@users.noreply.github.com>
2022-10-15 18:24:47 +0900

    Codelabs > Two Activities: Intent Extras
    Course: Android Developer Fundamentals
    Unit 1 > Lesson 2.1 > Task 3
    
    Intent object can pass data to target Activity in two ways
    - Intent Data   : a URI indicating the specific data to be acted on
    - Intent Extras : key-value pairs in a Bundle
    
    Intent Extras
    - to save   : intent.putExtra(key, value)
    - to load 1 : intent.get[Type]Extra(key)
    - to load 2 : intent.getExtras().get[Type](key)
    - to load 3 : intent.getExtras().get[Type](key, default)
    
    * key : String Type
    * intent.getExtras() return Bundle
    
    A Bundle is a collection of data, stored as key-value pairs
    
    Attribute of TextView
    - ems : set width to fit the width of the text
    * applied only when value of layout_width is wrap_content

--- codelabs_java_developers/src/main/java/com/codelabs/java_developers/fundamentals/Fundamentals021A.java
+++ codelabs_java_developers/src/main/java/com/codelabs/java_developers/fundamentals/Fundamentals021A.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,16 +13,30 @@ import com.codelabs.java_developers.R;
 public class Fundamentals021A extends AppCompatActivity {
 	private static final String LOG_TAG = Fundamentals021A.class.getSimpleName();
 
+	// set key to identify String value
+	public static final String EXTRA_MESSAGE = "com.codelabs.java_developers.extra.MESSAGE";
+
+	private EditText mMessageEditText;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fundamentals021_a);
+
+		mMessageEditText = findViewById(R.id.editText_main);
 	}
 
 	public void launchSecondActivity(View view) {
 		Log.d(LOG_TAG, "Button clicked!");
 
 		Intent intent = new Intent(this, Fundamentals021B.class);
+
+		// get String from EditText
+		String message = mMessageEditText.getText().toString();
+
+		// put key-value data in Intent Extras
+		intent.putExtra(EXTRA_MESSAGE, message);
+
 		startActivity(intent);
 	}
 }
--- codelabs_java_developers/src/main/java/com/codelabs/java_developers/fundamentals/Fundamentals021B.java
+++ codelabs_java_developers/src/main/java/com/codelabs/java_developers/fundamentals/Fundamentals021B.java
@@ -1,6 +1,8 @@
 package com.codelabs.java_developers.fundamentals;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,5 +14,14 @@ public class Fundamentals021B extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_fundamentals021_b);
+
+		// get Intent which starts this Activity
+		Intent intent = getIntent();
+
+		// receive String data in Intent Extra
+		String message = intent.getStringExtra(Fundamentals021A.EXTRA_MESSAGE);
+
+		TextView textView = findViewById(R.id.text_message);
+		textView.setText(message);
 	}
 }
